82b61ad23a9bf5354a5a322c7f4594b2ee33dc7f
unknown <unknown@example.com>
2022-07-20 08:26:36 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-9gptw10099911626","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-9gptw10099911626 --n quickstartFunctions --r --project "E:\LS" --report_first --report
